0c387fe9a47691aaa6c3ddec06165c149ed4387c
Naz1337 <69098093+Naz1337@users.noreply.github.com>
2025-05-28 08:15:05 +0800

    buang placeholder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,22 +2,6 @@
     <div class="max-w-280 ms-auto me-auto mt-20 rounded-box flex flex-col items-center p-4">
         <div class="flex flex-col items-center p-4 rounded-box w-220">
             <div class="flex flex-wrap gap-4 justify-start w-[784px]">
-                <div class="card bg-base-200 w-46">
-                    <figure>
-                        <img src="/imgs/mdrop.jpg" alt="" class="w-46 h-46">
-                    </figure>
-                    <div class="card-body">
-                        <h2 class="card-title">Moondrop Space Travel TWS</h2>
-                        <div>RM 99.99</div>
-
-                        <div class="divider m-0"></div>
-
-                        <div class="flex gap-2 text-sm items-baseline text-base-content/60">
-                            <x-icon.user-circle class="max-h-[1em] shrink-0 relative top-[0.125em]"/>
-                            <div>Red Ape Studio KingZ Kpoopers</div>
-                        </div>
-                    </div>
-                </div>
                 @foreach($products as $product)
                     <x-catalogue.item :$product />
                 @endforeach
